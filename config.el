c424e43935a677840d2fa90489c43f625dc19487
yejianfengblue <yejianfengblue@hotmail.com>
2022-11-20 16:47:31 +0800

    map spc-spc to M-x

--- config.el
+++ config.el
@@ -75,3 +75,8 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(map! :leader
+      :desc "M-x"                   "SPC"    #'execute-extended-command
+      :leader)
+
